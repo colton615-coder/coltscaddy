cb1d5687e0e1e233a8dddecddb417e9571b6c2e6
Colton Thomas <colton615@gmail.com>
2026-07-11 09:28:03 -0400

    Initial Commit

---
+++ COLTSCADDY/COLTSCADDYApp.swift
@@ -0,0 +1,17 @@
+//
+//  COLTSCADDYApp.swift
+//  COLTSCADDY
+//
+//  Created by Colton Thomas on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct COLTSCADDYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COLTSCADDYTests/COLTSCADDYTests.swift
@@ -0,0 +1,19 @@
+//
+//  COLTSCADDYTests.swift
+//  COLTSCADDYTests
+//
+//  Created by Colton Thomas on 7/11/26.
+//
+
+import Testing
+@testable import COLTSCADDY
+
+struct COLTSCADDYTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
